2a84254430309d325637096db0252f56cbea2dd7
Myunkyu Park <spica@wafflestudio.com>
2012-06-14 01:23:13 +0900

    only js version 거의 완료

--- quad_onlyjs.js
+++ quad_onlyjs.js
@@ -1,5 +1,52 @@
 (function(window, document, undefined) {
 
+  /**
+   * Utility function to create elements. If no tag name is given,
+   * a DIV is created. Optionally properties can be passed.
+   */
+  function createEl(tag, prop) {
+    var el = document.createElement(tag || 'div');
+    var n;
+
+    for(n in prop) {
+      el[n] = prop[n];
+		}
+
+    return el;
+  }
+
+  /**
+   * Appends children and returns the parent.
+   */
+  function insertChild(parent /* child1, child2, ...*/) {
+    for (var i=1, n=arguments.length; i<n; i++) {
+      parent.appendChild(arguments[i]);
+    }
+    return parent;
+  }
+
+  /**
+   * Sets multiple style properties at once.
+   */
+  function css(el, prop) {
+    for (var n in prop) {
+      el.style[vendor(el, n)||n] = prop[n];
+    }
+    return el;
+  }
+
+  /**
+   * Fills in default values.
+   */
+  function merge(obj) {
+    for (var i=1; i < arguments.length; i++) {
+      var def = arguments[i];
+      for (var n in def) {
+        if (obj[n] === undefined) obj[n] = def[n];
+      }
+    }
+    return obj;
+  }
 		function quadCurveAnimation(target, endPointX, endPointY, duration, isExpanding) {
 			console.log('endpoint x =');
 			console.log(endPointX);
@@ -7,24 +54,29 @@
 			console.log(endPointY);
 			offsetX = endPointX - parseFloat(target.style.left); 
 			offsetY = endPointY - parseFloat(target.style.top);
+			var dOpacity = -0.01;
 			if (isExpanding) {
 				target.style.display = 'block';
+				dOpacity = 0.01;
 			}
-			function doMove(i) {
-				//console.log(this.itemContainer.style.top);
-				if (i < 100) {
-				  target.style.opacity = parseFloat(target.style.opacity) + 1.0 / 100;
+			var i=0;	
+			function doMove(target, offsetX, offsetY, dOpacity, timeout) {
+				setTimeout( function() {
+					target.style.opacity = parseFloat(target.style.opacity) + dOpacity;
 					target.style.top = (parseFloat(target.style.top) + offsetY / 100) + 'px';
 					target.style.left = (parseFloat(target.style.left) + offsetX / 100) + 'px';
-					setTimeout(function(){ doMove(++i)}, duration / 100);
-				} else {
-					if (!isExpanding) {
-						target.style.display = 'none';
-					}
-					return;
-				}
+				}, timeout);
 			};
-			doMove(0);
+			//doMove(0);
+			var time = 0;
+			while(i++<100) {
+					time += duration / 100
+					doMove(target, offsetX, offsetY, dOpacity, time);	
+			}
+			if (!isExpanding) {
+				console.log('disappear');
+				target.style.display = 'none';
+			}
 	}
 
 
@@ -63,15 +115,6 @@
 			item.src = self.attributes['imageSrc'];
 			item.onclick = self.attributes['bindFunc'];
 			self.itemContainer = item;  
-//			var item = '<img src="' + img + '" id="' + this.attributes['id'] + '" style="position:absolute; opacity:0; cursor: pointer;">';
-	//		$('body').append(item);
-	//		this.itemContainer = $('#' + this.attributes['id']);
-
-	//		this.itemContainer.css('z-index', 10000);
-			
-	//		this.itemContainer.bind('click', function(){
-//					self.bindingFunc();
-//			});
 		};
 
 		this.setTargetMenu = function(target) {
@@ -86,9 +129,7 @@
 
 		this.setStartPnt = function(startPnt) {
 			self.attributes['startPoint'] = startPnt;
-			//this.itemContainer.css('top', this.attributes['startPoint']['y']);
 			self.itemContainer.style.top = self.attributes['startPoint']['y'] + 'px';
-			//this.itemContainer.css('left', this.attributes['startPoint']['x']);
 			self.itemContainer.style.left = self.attributes['startPoint']['x'] + 'px';
 		};
 
@@ -96,15 +137,11 @@
 			self.attributes['endPoint'] = endPnt;
 		};
 
-		this.expand = function(duration, easing) {
-			//this.itemContainer.show();
-			//this.itemContainer.animate({"left" : this.attributes['endPoint']['x'] , "top" : this.attributes['endPoint']['y'], "opacity": 1}, duration, easing);
+		this.expand = function(duration) {
 			quadCurveAnimation(self.itemContainer, self.attributes['endPoint']['x'], self.attributes['endPoint']['y'], duration, true);
 		};
 
-		this.close = function(duration, easing) {
-			//this.itemContainer.animate({"left" : this.attributes['startPoint']['x'] , "top" : this.attributes['startPoint']['y'], "opacity": 0}, duration, easing);
-			//this.itemContainer.hide(duration);
+		this.close = function(duration) {
 			quadCurveAnimation(self.itemContainer, this.attributes['startPoint']['x'], this.attributes['startPoint']['y'], duration, false);
 		};
 
@@ -148,37 +185,13 @@
 
 		this.init = function(options) {
 			self.attributes = $.extend(this.defaults, options);
-			//this.menus = options.menus;
-			/*
-			this.menuContainer = $('#' + this.attributes['id']);
-	
-			this.menuContainer.bind('click', function() {
-				self.setClick();
-			});
-			*/
-
-			/*
-			this.menuContainer.css('cursor', 'pointer').css('color', '#2B4CCF');
-
-			if (this.attributes['startPoint']['x'] == -1)
-			{
-				var	startY = this.menuContainer.offset()['top'] - this.menuContainer.height() / 2;
-				var startX = this.menuContainer.offset()['left'] + this.menuContainer.width() / 2;
-				var startPnt = {'x' : startX, 'y': startY};
-				this.attributes['startPoint'] = startPnt;
-			}  */
 		};
 
 		this.quadcurve = function(target) {
 			this.menuContainer = target;
 			this.menuContainer.onclick = self.setClick;
-		  /*
-			this.menuContainer.bind('click', function() {
-				self.setClick();
-			});  */
 			this.menuContainer.style.cursor = 'pointer';
 			this.menuContainer.style.color = '#2B4CFF';
-			//this.menuContainer.css('cursor', 'pointer').css('color', '#2B4CCF');
 
 			if (this.attributes['startPoint']['x'] == -1)
 			{
@@ -193,41 +206,26 @@
 			this.attributes['closeButtonId'] = this.attributes['id'] + '_close';
 
 			var closeButton = document.createElement('span');
-			//closeButton.setAttribute('id', this.attributes['closeButtonId']);
 			closeButton.id = this.attributes['closeButtonId'];
 			closeButton.style.position = 'absolute';
+			closeButton.style.display = 'none';
 			closeButton.style.opacity = 0;
 			closeButton.style.cursor = 'pointer';
 			closeButton.onclick = self._close;
-			//closeButton.setAttributes('onclick', self._close);
-
-			//closeButton.setAttribute('style', 'position:absolute; opacity:0; cursor:pointer;');
 
 			if (this.attributes['closeButtonImgSrc'] == '')
 			{
-				//closeButton.appendChild(document.createTextNode('x'));
 				closeButton.innerHTML = 'x'
-				//closeButton = '<span id="' + this.attributes['closeButtonId'] + '" style="position:absolute; opacity:0; cursor: pointer;">x</span>';
 			} else {
 				var closeButtonImg = document.createElement('img');
-				//closeButtonImg.setAttribute('id', this.attributes['closeButtonId'] + '_image');
 				closeButtonImg.id = self.attributes['closeButtonId'] + '_image';
-				//closeButtonImg.setAttribute('src', this.attributes['closeButtonImgSrc']);
 				closeButtonImg.src = self.attributes['closeButtonImgSrc'];
 				closeButton.appendChild(closeButtonImg);
-				//closeButton = '<img id="' + this.attributes['closeButtonId'] + '" src="' + this.attributes['closeButtonImg'] + '" style="position:absolute; opacity:0; cursor:pointer;">';
 			}
-			//var closeButton = '<span id="' + this.attributes['closeButtonId'] + '" style="position:absolute; opacity:0; cursor: pointer;">x</span>';
-			
-			//$('body').append(closeButton);
-
 			this.closeButtonContainer = closeButton;
 
 			this.menuContainer.appendChild(closeButton);
-			/*
-			$('#' + this.attributes['closeButtonId']).bind('click', function(){
-				self._close();
-			}); */
+
 			self._setMenu();
 		};
 
@@ -240,19 +238,16 @@
 				var item = new QuadCurveMenuItem(self.attributes.menuItemOptions[i]);
 				item.setTargetMenu(self.menuContainer);
 				item.setStartPnt(startPnt);
-				//item.attributes['menuId'] = this.attributes['id'];
 				var endX = parseInt(startX) + self.attributes['endRadius'] * Math.sin(i * self.attributes['menuWholeAngle'] / count);
 				var endY = parseInt(startY) - self.attributes['endRadius'] * Math.cos(i * self.attributes['menuWholeAngle'] / count);
 				var endPnt = {x: endX, y:endY};
 				item.setEndPnt(endPnt);
-				//item.attributes['endPoint']['y'] = endY;
 				self.attributes.menuItems.push(item);
 			}
+			console.log(self.attributes.menuItems);
 			// close Button 수정. 
-			//this.closeButtonContainer.setAttribute('style', this.closeButtonConatiner.style + 'top: ' + startY + '; left:' + startX + ';');
 			self.closeButtonContainer.style.top = startY + 'px';
 			self.closeButtonContainer.style.left = startX + 'px';
-			//$('#'+this.attributes['closeButtonId']).css('top', startY).css('left', startX);
 			this.attributes['closeButtonEndPoint']['x'] = parseInt(startX) + 1.4 *  this.attributes['endRadius'] * Math.sin(this.attributes['menuWholeAngle'] / 2);		
 			this.attributes['closeButtonEndPoint']['y'] = parseInt(startY) - 1.4 *  this.attributes['endRadius'] * Math.cos(this.attributes['menuWholeAngle'] / 2);		
 		};
@@ -266,31 +261,9 @@
 				var count = this.attributes.menuItems.length;
 				for (var i=0; i<count; i++) {
 					var item = this.attributes.menuItems[i];
-					item.expand(this.attributes['expandDuration'], this.attributes['easing']);
+					item.expand(this.attributes['expandDuration']);
 				}
-				//$('#'+this.attributes['closeButtonId']).show();
-				//this.closeButtonContainer.style.opacity = 1;
-				//
-				var offsetX = this.attributes['closeButtonEndPoint']['x'] - parseFloat(this.closeButtonContainer.style.left);
-				var offsetY = this.attributes['closeButtonEndPoint']['y'] - parseFloat(this.closeButtonContainer.style.top);
-				var i = 0
-				function doMove() {
-					//console.log(offsetX);
-					//console.log(self.closeButtonContainer.style);
-					if (i < 100) {
-						//console.log(self.closeButtonContainer.style.opacity);
-						self.closeButtonContainer.style.opacity = parseFloat(self.closeButtonContainer.style.opacity) + 1.0 / 100;
-						self.closeButtonContainer.style.top = (parseFloat(self.closeButtonContainer.style.top) + offsetY / 100) + 'px';
-						self.closeButtonContainer.style.left = (parseFloat(self.closeButtonContainer.style.left) +offsetX / 100) + 'px';
-						i += 1
-						setTimeout(doMove, 0.1);
-						} else {
-							return;
-						}
-				};
-
-				doMove();
-				//$('#'+this.attributes['closeButtonId']).animate({"left" : this.attributes['closeButtonEndPoint']['x'] , "top" : this.attributes['closeButtonEndPoint']['y'], "opacity": 1}, this.attributes['expandDuration'], this.attributes['easing']);
+				quadCurveAnimation(self.closeButtonContainer, this.attributes['closeButtonEndPoint']['x'], this.attributes['closeButtonEndPoint']['y'], this.attributes['expandDuration'], true);
 				this.attributes['expanding'] = true;
 			}
 		};
@@ -302,8 +275,7 @@
 					var item = self.attributes.menuItems[i];
 					item.close(self.attributes['closeDuration']);
 				}
-				$('#'+this.attributes['closeButtonId']).animate({"left" : this.attributes['startPoint']['x'] , "top" : this.attributes['startPoint']['y'], "opacity": 0}, this.attributes['closeDuration']);
-				$('#'+this.attributes['closeButtonId']).hide(this.attributes['closeDuration']);
+				quadCurveAnimation(self.closeButtonContainer, this.attributes['startPoint']['x'], this.attributes['startPoint']['y'], this.attributes['expandDuration'], false);
 				this.attributes['expanding'] = false;
 			}
 		};
